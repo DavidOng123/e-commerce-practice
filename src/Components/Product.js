f8fb567b2fa9a738642c1c855afc84e0eb1ef5c7
David Ong <lipweiong@gmail.com>
2024-01-07 12:30:19 +0800

    change the props

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -11,9 +11,7 @@ export default ()=>{
         <div className="row">
         {state.products.slice(0, state.products.length).map((item, index) => (
           <div key={index}>
-            <Link to={`/product/${item.id}`}>
-            <RecommendationItem title={item.title} content={item.content} />
-            </Link>
+            <RecommendationItem item={item}/>
           </div>
         ))}
       </div>
